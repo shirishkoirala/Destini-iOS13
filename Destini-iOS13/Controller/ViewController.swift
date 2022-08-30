739f7b8f014c164392e75fa5eaef517e9951caae
Shirish Koirala <shirishkoirala@gmail.com>
2022-08-30 11:38:17 +1000

    Challenge Completed with MVC pattern.

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        storyBrain.checkAnswer(sender.currentTitle!)
+        updateUI()
+    }
+    
+    func updateUI(){
+        storyLabel.text = storyBrain.currentStory.title
+        choice1Button.setTitle(storyBrain.currentStory.choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.currentStory.choice2, for: .normal)
+    }
+    
 }
 
